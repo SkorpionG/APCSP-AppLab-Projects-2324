81d3bbdc652fe8363c3d58ff0d2d37b355e2bee0
Skorpion G2000 <105552971+SkorpionG@users.noreply.github.com>
2024-06-08 14:14:21 -0700

    Project init

---
+++ projectInfo.js
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        title: "Project 1",
+        description: "Description for project 1.",
+        image: "https://via.placeholder.com/150",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiaUfaIcAe9irzPwtIzvj-uU"
+    },
+    {
+        title: "Project 2",
+        description: "Description for project 2.",
+        image: "https://via.placeholder.com/150",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiU9a7mr_qQ__tIzDfpof7-g"
+    },
+    {
+        title: "Project 3",
+        description: "Description for project 3.",
+        image: "https://via.placeholder.com/150",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiYvKGAhL12KB31v-ZyVkTpo"
+    },
+    {
+        title: "Project 1",
+        description: "Description for project 1.",
+        image: "https://via.placeholder.com/150",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiaUfaIcAe9irzPwtIzvj-uU"
+    },
+    {
+        title: "Project 2",
+        description: "Description for project 2.",
+        image: "https://via.placeholder.com/150",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiU9a7mr_qQ__tIzDfpof7-g"
+    },
+    {
+        title: "Project 3",
+        description: "Description for project 3.",
+        image: "https://via.placeholder.com/150",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiYvKGAhL12KB31v-ZyVkTpo"
+    }
+]
