d3c5bda240017bba9266677bb68bdd5ab2129003
Skorpion G2000 <105552971+SkorpionG@users.noreply.github.com>
2024-06-08 17:16:24 -0700

    update assets and code

--- projectInfo.js
+++ projectInfo.js
@@ -1,38 +1,164 @@
 export const projects = [
     {
-        title: "Project 1",
-        description: "Description for project 1.",
-        image: "https://via.placeholder.com/150",
+        title: "Mood Minder",
+        description: "",
+        image: "./assets/mood_minder_thumbnail.png",
         url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiaUfaIcAe9irzPwtIzvj-uU"
     },
     {
-        title: "Project 2",
-        description: "Description for project 2.",
-        image: "https://via.placeholder.com/150",
+        title: "Museum Ticket Generator",
+        description: "",
+        image: "./assets/museum_ticket_generator_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMieUwc7LmDHCkR0OB0jUsG04"
+    },
+    {
+        title: "Planet Explorer",
+        description: "This app lets you explore the eight planets of the solar system.",
+        image: "./assets/planet_explorer_thumbnail.png",
         url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiU9a7mr_qQ__tIzDfpof7-g"
     },
     {
-        title: "Project 3",
+        title: "Video Game Explorer",
+        description: "Find your next game to play!",
+        image: "./assets/video_game_explorer_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbgBHvlNsMfp2PrZ8aE5_5eI"
+    },
+    {
+        title: "Ticket App",
+        description: "Description for project 2.",
+        image: "./assets/ticket_app_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiSAzZUPLJ6CIL3ecqVTbFiA"
+    },
+    {
+        title: "Tic Tac Toe",
         description: "Description for project 3.",
-        image: "https://via.placeholder.com/150",
+        image: "./assets/tic_tac_toe_thumbnail.png",
         url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiYvKGAhL12KB31v-ZyVkTpo"
     },
     {
-        title: "Project 1",
+        title: "The Memory Game",
+        description: "See how well and how long you can remember!",
+        image: "./assets/the_memory_game_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/kG4_0ij9ybAlm2v0kA8cfhu1QhhvKZoAdGo2utoGE0s"
+    },
+    {
+        title: "Sentence Randomizer",
+        description: "Description for project 2.",
+        image: "./assets/sentence_randomizer_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbjizSxjl9iFH_Gi6haV_HN0"
+    },
+    {
+        title: "Screen Locker App",
+        description: "Description for project 3.",
+        image: "./assets/screen_locker_app_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbhzD5t1uq9K4ujAxDW0K59Y"
+    },
+    {
+        title: "Rock Paper Scissors",
         description: "Description for project 1.",
-        image: "https://via.placeholder.com/150",
+        image: "./assets/rock_paper_scissors_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbu7M-MAC_DK0xqpTuqKwuc0"
+    },
+    {
+        title: "RemindME",
+        description: "Description for project 2.",
+        image: "./assets/remind_me_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbpu0l7aO05tMYsf2lvVkWbQ"
+    },
+    {
+        title: "Reminder App",
+        description: "Description for project 3.",
+        image: "./assets/reminder_app_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbjT8Nsi5_963nK3EDSjWfVU"
+    },
+    {
+        title: "Random Forecaster",
+        description: "Description for project 1.",
+        image: "./assets/random_forecaster_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbvbigCi94zM4F2lRVv0POI4"
+    },
+    {
+        title: "Random Country Generator",
+        description: "Description for project 2.",
+        image: "./assets/random_country_generator_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/oa_p2UxyGadRpKWvnlxZW_s-UQo6WGrLkRn_tdbM-ho"
+    },
+    {
+        title: "Quote Maker",
+        description: "Description for project 3.",
+        image: "./assets/quote_maker_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiZEg1qR_n74b7q5F016miyQ"
+    },
+    {
+        title: "Photo Encouragement App",
+        description: "Description for project 1.",
+        image: "./assets/photo_encouragement_app_thumbnail.png",
         url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiaUfaIcAe9irzPwtIzvj-uU"
     },
     {
-        title: "Project 2",
+        title: "Music Player",
         description: "Description for project 2.",
-        image: "https://via.placeholder.com/150",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiU9a7mr_qQ__tIzDfpof7-g"
+        image: "./assets/music_player_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbktrunUHKT4dNlIK2wxc1sM"
     },
     {
-        title: "Project 3",
+        title: "Hydration Tracker",
         description: "Description for project 3.",
-        image: "https://via.placeholder.com/150",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiYvKGAhL12KB31v-ZyVkTpo"
+        image: "./assets/hydration_tracker_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMibVJLzMQPwa183ggVQKxL3k"
+    },
+    {
+        title: "Grades.ly",
+        description: "Description for project 1.",
+        image: "./assets/gradesly_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbkXbGf2EQVDGJhijZa2uLS0"
+    },
+    {
+        title: "Gas Buddy",
+        description: "Description for project 2.",
+        image: "./assets/gas_buddy_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbqKmgq7EZW95gBcrQpKnE6c"
+    },
+    {
+        title: "Follow The Sequence Game",
+        description: "Description for project 3.",
+        image: "./assets/follow_the_sequence_game_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbo8o_aiZaoXAqE217y_9qmk"
+    },
+    {
+        title: "Flip Card Game",
+        description: "Description for project 1.",
+        image: "./assets/flip_card_game_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/oa_p2UxyGadRpKWvnlxZW6XgJMZ2cOwJb5AjCjf1bnY"
+    },
+    {
+        title: "Emoji Maker",
+        description: "Description for project 2.",
+        image: "./assets/emoji_maker_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbndJPIKXHzBhxoX3L4wHgco"
+    },
+    {
+        title: "Dice Roller",
+        description: "Description for project 3.",
+        image: "./assets/dice_roller_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbhUl4CD3EN09mXNQJo0iUaM"
+    },
+    {
+        title: "Breakfast Picker",
+        description: "Description for project 1.",
+        image: "./assets/breakfast_picker_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiZBLQacdQ_8_Yz5TMvILJ_s"
+    },
+    {
+        title: "Basketball Stat Keeper",
+        description: "Description for project 2.",
+        image: "./assets/basketball_stat_keeper_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiQjAg6LjZVSC3gw1fqO1zN8"
+    },
+    {
+        title: "Air Quality Tracker",
+        description: "Description for project 3.",
+        image: "./assets/air_quality_tracker_thumbnail.png",
+        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbkjcXf5CWjWwMaxIEEjyjmA"
     }
 ]
