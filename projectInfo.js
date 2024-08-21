a210fc363dbed99981b52610526a30ae2a1b4f49
SkorpionG2000 <105552971+SkorpionG@users.noreply.github.com>
2024-08-21 13:30:27 -0700

    Add canvas nest, page info, and format code files.

--- projectInfo.js
+++ projectInfo.js
@@ -1,164 +1,177 @@
 export const projects = [
-    {
-        title: "Mood Minder",
-        description: "A simple app that are part of the class curriculum.",
-        image: "./assets/mood_minder_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiaUfaIcAe9irzPwtIzvj-uU"
-    },
-    {
-        title: "Museum Ticket Generator",
-        description: "A simple app that are part of the class curriculum, featuring conditional statements.",
-        image: "./assets/museum_ticket_generator_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMieUwc7LmDHCkR0OB0jUsG04"
-    },
-    {
-        title: "Planet Explorer",
-        description: "This app lets you explore the eight planets of the solar system.",
-        image: "./assets/planet_explorer_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiU9a7mr_qQ__tIzDfpof7-g"
-    },
-    {
-        title: "Video Game Explorer",
-        description: "Find your next game to play!",
-        image: "./assets/video_game_explorer_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbgBHvlNsMfp2PrZ8aE5_5eI"
-    },
-    {
-        title: "Ticket App",
-        description: "Conditional statements practice project.",
-        image: "./assets/ticket_app_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiSAzZUPLJ6CIL3ecqVTbFiA"
-    },
-    {
-        title: "Tic Tac Toe",
-        description: "Play against your friends or yourself!",
-        image: "./assets/tic_tac_toe_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiYvKGAhL12KB31v-ZyVkTpo"
-    },
-    {
-        title: "The Memory Game",
-        description: "See how well and how long you can remember!",
-        image: "./assets/the_memory_game_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/kG4_0ij9ybAlm2v0kA8cfhu1QhhvKZoAdGo2utoGE0s"
-    },
-    {
-        title: "Sentence Randomizer",
-        description: "Generate sentence with random font style and color.",
-        image: "./assets/sentence_randomizer_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbjizSxjl9iFH_Gi6haV_HN0"
-    },
-    {
-        title: "Screen Locker App",
-        description: "An app that simulate the phone's screen lock feature.",
-        image: "./assets/screen_locker_app_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbhzD5t1uq9K4ujAxDW0K59Y"
-    },
-    {
-        title: "Rock Paper Scissors",
-        description: "Play against the computer, and the one who gets 10 wins first wins!",
-        image: "./assets/rock_paper_scissors_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbu7M-MAC_DK0xqpTuqKwuc0"
-    },
-    {
-        title: "RemindME",
-        description: "An app that reminds you to do something by adding a reminder to a list.",
-        image: "./assets/remind_me_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbpu0l7aO05tMYsf2lvVkWbQ"
-    },
-    {
-        title: "Reminder App",
-        description: "Keep track of your to-dos and reminders with this app.",
-        image: "./assets/reminder_app_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbjT8Nsi5_963nK3EDSjWfVU"
-    },
-    {
-        title: "Random Forecaster",
-        description: "Get the weather forecast for a random city. Because the weather data is from a dataset that doesn't have the latest data, this program doesn't work as expected.",
-        image: "./assets/random_forecaster_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbvbigCi94zM4F2lRVv0POI4"
-    },
-    {
-        title: "Random Country Generator",
-        description: "Generate a country name.",
-        image: "./assets/random_country_generator_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/oa_p2UxyGadRpKWvnlxZW_s-UQo6WGrLkRn_tdbM-ho"
-    },
-    {
-        title: "Quote Maker",
-        description: "Type in a line of text, choose the size, font, and color, and get a new quote!",
-        image: "./assets/quote_maker_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiZEg1qR_n74b7q5F016miyQ"
-    },
-    {
-        title: "Photo Encouragement App",
-        description: "I don't know what this app is for, but I just want to keep it.",
-        image: "./assets/photo_encouragement_app_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJblgM6plguu9hQa9kUFTuf8Y"
-    },
-    {
-        title: "Music Player",
-        description: "A simple music player that plays a default song that I don't know the name.",
-        image: "./assets/music_player_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbktrunUHKT4dNlIK2wxc1sM"
-    },
-    {
-        title: "Hydration Tracker",
-        description: "A simple hydration tracker that keeps track of your hydration levels when you drink water and work out.",
-        image: "./assets/hydration_tracker_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMibVJLzMQPwa183ggVQKxL3k"
-    },
-    {
-        title: "Grades.ly",
-        description: "A simple average grade calculator.",
-        image: "./assets/gradesly_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbkXbGf2EQVDGJhijZa2uLS0"
-    },
-    {
-        title: "Gas Buddy",
-        description: "Get the historical gas prices in 7 major U.S. cities, from 2003 to 2023.",
-        image: "./assets/gas_buddy_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbqKmgq7EZW95gBcrQpKnE6c"
-    },
-    {
-        title: "Follow The Sequence Game",
-        description: "Similar to the game \"Follow the Sequence\", but with completely different sequence each level.",
-        image: "./assets/follow_the_sequence_game_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbo8o_aiZaoXAqE217y_9qmk"
-    },
-    {
-        title: "Flip Card Game",
-        description: "Flip the cards to test your memory!",
-        image: "./assets/flip_card_game_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/oa_p2UxyGadRpKWvnlxZW6XgJMZ2cOwJb5AjCjf1bnY"
-    },
-    {
-        title: "Emoji Maker",
-        description: "Make your own unique emoji with this app!",
-        image: "./assets/emoji_maker_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbndJPIKXHzBhxoX3L4wHgco"
-    },
-    {
-        title: "Dice Roller",
-        description: "Randomly roll a dice between 1 and 6 for 20 times, can calculate how many odd and even numbers are rolled.",
-        image: "./assets/dice_roller_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbhUl4CD3EN09mXNQJo0iUaM"
-    },
-    {
-        title: "Breakfast Picker",
-        description: "Trouble finding the perfect breakfast? Try this app!",
-        image: "./assets/breakfast_picker_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiZBLQacdQ_8_Yz5TMvILJ_s"
-    },
-    {
-        title: "Basketball Stat Keeper",
-        description: "An simple app that tracks the stats of a basketball player during a game. (part of the class curriculum)",
-        image: "./assets/basketball_stat_keeper_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiQjAg6LjZVSC3gw1fqO1zN8"
-    },
-    {
-        title: "Air Quality Tracker",
-        description: "Select a state to get its air quality data in 2020.",
-        image: "./assets/air_quality_tracker_thumbnail.png",
-        url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbkjcXf5CWjWwMaxIEEjyjmA"
-    }
-]
+  {
+    title: "Mood Minder",
+    description: "A simple app that are part of the class curriculum.",
+    image: "./assets/mood_minder_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiaUfaIcAe9irzPwtIzvj-uU",
+  },
+  {
+    title: "Museum Ticket Generator",
+    description:
+      "A simple app that are part of the class curriculum, featuring conditional statements.",
+    image: "./assets/museum_ticket_generator_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMieUwc7LmDHCkR0OB0jUsG04",
+  },
+  {
+    title: "Planet Explorer",
+    description:
+      "This app lets you explore the eight planets of the solar system.",
+    image: "./assets/planet_explorer_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiU9a7mr_qQ__tIzDfpof7-g",
+  },
+  {
+    title: "Video Game Explorer",
+    description: "Find your next game to play!",
+    image: "./assets/video_game_explorer_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbgBHvlNsMfp2PrZ8aE5_5eI",
+  },
+  {
+    title: "Ticket App",
+    description: "Conditional statements practice project.",
+    image: "./assets/ticket_app_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiSAzZUPLJ6CIL3ecqVTbFiA",
+  },
+  {
+    title: "Tic Tac Toe",
+    description: "Play against your friends or yourself!",
+    image: "./assets/tic_tac_toe_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiYvKGAhL12KB31v-ZyVkTpo",
+  },
+  {
+    title: "The Memory Game",
+    description: "See how well and how long you can remember!",
+    image: "./assets/the_memory_game_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/kG4_0ij9ybAlm2v0kA8cfhu1QhhvKZoAdGo2utoGE0s",
+  },
+  {
+    title: "Sentence Randomizer",
+    description: "Generate sentence with random font style and color.",
+    image: "./assets/sentence_randomizer_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbjizSxjl9iFH_Gi6haV_HN0",
+  },
+  {
+    title: "Screen Locker App",
+    description: "An app that simulate the phone's screen lock feature.",
+    image: "./assets/screen_locker_app_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbhzD5t1uq9K4ujAxDW0K59Y",
+  },
+  {
+    title: "Rock Paper Scissors",
+    description:
+      "Play against the computer, and the one who gets 10 wins first wins!",
+    image: "./assets/rock_paper_scissors_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbu7M-MAC_DK0xqpTuqKwuc0",
+  },
+  {
+    title: "RemindME",
+    description:
+      "An app that reminds you to do something by adding a reminder to a list.",
+    image: "./assets/remind_me_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbpu0l7aO05tMYsf2lvVkWbQ",
+  },
+  {
+    title: "Reminder App",
+    description: "Keep track of your to-dos and reminders with this app.",
+    image: "./assets/reminder_app_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbjT8Nsi5_963nK3EDSjWfVU",
+  },
+  {
+    title: "Random Forecaster",
+    description:
+      "Get the weather forecast for a random city. Because the weather data is from a dataset that doesn't have the latest data, this program doesn't work as expected.",
+    image: "./assets/random_forecaster_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbvbigCi94zM4F2lRVv0POI4",
+  },
+  {
+    title: "Random Country Generator",
+    description: "Generate a country name.",
+    image: "./assets/random_country_generator_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/oa_p2UxyGadRpKWvnlxZW_s-UQo6WGrLkRn_tdbM-ho",
+  },
+  {
+    title: "Quote Maker",
+    description:
+      "Type in a line of text, choose the size, font, and color, and get a new quote!",
+    image: "./assets/quote_maker_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiZEg1qR_n74b7q5F016miyQ",
+  },
+  {
+    title: "Photo Encouragement App",
+    description:
+      "I don't know what this app is for, but I just want to keep it.",
+    image: "./assets/photo_encouragement_app_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJblgM6plguu9hQa9kUFTuf8Y",
+  },
+  {
+    title: "Music Player",
+    description:
+      "A simple music player that plays a default song that I don't know the name.",
+    image: "./assets/music_player_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbktrunUHKT4dNlIK2wxc1sM",
+  },
+  {
+    title: "Hydration Tracker",
+    description:
+      "A simple hydration tracker that keeps track of your hydration levels when you drink water and work out.",
+    image: "./assets/hydration_tracker_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMibVJLzMQPwa183ggVQKxL3k",
+  },
+  {
+    title: "Grades.ly",
+    description: "A simple average grade calculator.",
+    image: "./assets/gradesly_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbkXbGf2EQVDGJhijZa2uLS0",
+  },
+  {
+    title: "Gas Buddy",
+    description:
+      "Get the historical gas prices in 7 major U.S. cities, from 2003 to 2023.",
+    image: "./assets/gas_buddy_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbqKmgq7EZW95gBcrQpKnE6c",
+  },
+  {
+    title: "Follow The Sequence Game",
+    description:
+      'Similar to the game "Follow the Sequence", but with completely different sequence each level.',
+    image: "./assets/follow_the_sequence_game_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbo8o_aiZaoXAqE217y_9qmk",
+  },
+  {
+    title: "Flip Card Game",
+    description: "Flip the cards to test your memory!",
+    image: "./assets/flip_card_game_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/oa_p2UxyGadRpKWvnlxZW6XgJMZ2cOwJb5AjCjf1bnY",
+  },
+  {
+    title: "Emoji Maker",
+    description: "Make your own unique emoji with this app!",
+    image: "./assets/emoji_maker_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbndJPIKXHzBhxoX3L4wHgco",
+  },
+  {
+    title: "Dice Roller",
+    description:
+      "Randomly roll a dice between 1 and 6 for 20 times, can calculate how many odd and even numbers are rolled.",
+    image: "./assets/dice_roller_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbhUl4CD3EN09mXNQJo0iUaM",
+  },
+  {
+    title: "Breakfast Picker",
+    description: "Trouble finding the perfect breakfast? Try this app!",
+    image: "./assets/breakfast_picker_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiZBLQacdQ_8_Yz5TMvILJ_s",
+  },
+  {
+    title: "Basketball Stat Keeper",
+    description:
+      "An simple app that tracks the stats of a basketball player during a game. (part of the class curriculum)",
+    image: "./assets/basketball_stat_keeper_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/KNoqYHL9nI_C-KUWjKAMiQjAg6LjZVSC3gw1fqO1zN8",
+  },
+  {
+    title: "Air Quality Tracker",
+    description: "Select a state to get its air quality data in 2020.",
+    image: "./assets/air_quality_tracker_thumbnail.png",
+    url: "https://studio.code.org/projects/applab/PwqCD-Cd-Oi3VMk5XhbJbkjcXf5CWjWwMaxIEEjyjmA",
+  },
+];
--- script.js
+++ script.js
@@ -3,50 +3,49 @@ import { projects } from "./projectInfo.js";
 let openInNewTab = false;
 
 function createProjectCard(project) {
-    const card = document.createElement('div');
-    card.className = 'project-card';
-    card.onclick = () => {
-        if (openInNewTab) {
-            window.open(project.url, '_blank');
-        } else {
-            window.location.href = project.url;
-        }
-    };
+  const card = document.createElement("div");
+  card.className = "project-card";
+  card.onclick = () => {
+    if (openInNewTab) {
+      window.open(project.url, "_blank");
+    } else {
+      window.location.href = project.url;
+    }
+  };
 
-    const img = document.createElement('img');
-    img.src = project.image;
-    card.appendChild(img);
+  const img = document.createElement("img");
+  img.src = project.image;
+  card.appendChild(img);
 
-    const content = document.createElement('div');
-    content.className = 'content';
+  const content = document.createElement("div");
+  content.className = "content";
 
-    const title = document.createElement('h2');
-    title.textContent = project.title;
-    content.appendChild(title);
+  const title = document.createElement("h2");
+  title.textContent = project.title;
+  content.appendChild(title);
 
-    const desc = document.createElement('p');
-    desc.textContent = project.description;
-    content.appendChild(desc);
+  const desc = document.createElement("p");
+  desc.textContent = project.description;
+  content.appendChild(desc);
 
-    card.appendChild(content);
+  card.appendChild(content);
 
-    return card;
+  return card;
 }
-
 
 function loadProjects() {
-    const container = document.getElementById('projects-container');
-    projects.forEach(project => {
-        const card = createProjectCard(project);
-        container.appendChild(card);
-    });
+  const container = document.getElementById("projects-container");
+  projects.forEach((project) => {
+    const card = createProjectCard(project);
+    container.appendChild(card);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    loadProjects();
+document.addEventListener("DOMContentLoaded", () => {
+  loadProjects();
 
-    const toggleSwitch = document.getElementById('toggle-new-tab');
-    toggleSwitch.addEventListener('change', (event) => {
-        openInNewTab = event.target.checked;
-    });
+  const toggleSwitch = document.getElementById("toggle-new-tab");
+  toggleSwitch.addEventListener("change", (event) => {
+    openInNewTab = event.target.checked;
+  });
 });
